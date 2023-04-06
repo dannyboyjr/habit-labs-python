03041d592ceae95e4d0e7d43ae3dbcc5ea9f5ffd
Dan Kimball <dankimball@Dans-MacBook-Pro-2.local>
2023-04-05 18:57:29 -0600

    incomplete log

--- app/api/incomplete_log_routes.py
+++ app/api/incomplete_log_routes.py
@@ -2,6 +2,8 @@ from flask import Blueprint, jsonify, session, request
 from flask_login import login_required, current_user
 from app.models import Habit,Journal, IncompleteLog, db
 from sqlalchemy import func
+from datetime import datetime, timedelta
+import pytz
 
 incomplete_log_routes = Blueprint('incomplete_logs', __name__)
 
@@ -30,3 +32,42 @@ def create_incomplete_log():
     db.session.commit()
     
     return jsonify(new_log.to_dict())
+
+
+@incomplete_log_routes.route('/summary', methods=['GET'])
+@login_required
+def get_summary_incomplete_logs():
+   timezone = pytz.timezone(current_user.timezone)
+   now = datetime.now(timezone)
+   year_start = datetime(now.year, 1, 1, tzinfo=timezone)
+   month_start = datetime(now.year, now.month, 1, tzinfo=timezone)
+   week_start = now - timedelta(days=now.weekday())
+   today_start = datetime(now.year, now.month, now.day, tzinfo=timezone)
+
+
+   def get_total_amount(start_date, end_date=None):
+       query = IncompleteLog.query.filter(
+           IncompleteLog.user_id == current_user.id,
+           IncompleteLog.created_at >= start_date
+       )
+       if end_date:
+           query = query.filter(IncompleteLog.created_at <= end_date)
+
+
+       return sum(log.amount for log in query.all())
+
+
+   total_amount = get_total_amount(datetime.min.replace(tzinfo=timezone))
+   total_year = get_total_amount(year_start)
+   total_month = get_total_amount(month_start)
+   total_week = get_total_amount(week_start)
+   total_today = get_total_amount(today_start, datetime.now(timezone))
+
+
+   return jsonify({
+       "total_amount": float(total_amount),
+       "total_year": float(total_year),
+       "total_month": float(total_month),
+       "total_week": float(total_week),
+       "total_today": float(total_today),
+   })
